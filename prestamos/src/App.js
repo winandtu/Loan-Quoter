6088b38c6b51110073f45228d04b28ec2977ea00
winandtu <jyfy121@gmail.com>
2023-12-04 23:14:54 -0500

    Extracción de datos del Dom
    Extracción de datos del Dom y verificación de errores

--- prestamos/src/App.js
+++ prestamos/src/App.js
@@ -1,11 +1,18 @@
 //import logo from './logo.svg';
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import './App.css';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
 
+
+
 //llamo a los componentes
 function App() {
+
+  //definir el estado:
+  const [cantidad, guardarCantidad] = useState(0);
+
+  const [plazo, guardarPlazo] = useState(''); //string vacio dado que los valores de las options estan en string
   return (
     //<div className="App">
     <Fragment>
@@ -15,7 +22,13 @@ function App() {
       />
 
       <div className='container'>
-        <Formulario/>
+        <Formulario
+
+        cantidad={cantidad}
+        guardarCantidad={guardarCantidad}
+        plazo = {plazo}
+        guardarPlazo = {guardarPlazo}
+        />
       </div>
 
     </Fragment>
--- prestamos/src/componentes/Formulario.js
+++ prestamos/src/componentes/Formulario.js
@@ -1,39 +1,76 @@
-import React from 'react'
+import React, {Fragment,useState} from 'react'
 
-const Formulario = () => {
-    return ( 
-        <form>
-          <div className="row">
-              <div>
-                  <label>Cantidad Prestamo</label>
-                  <input 
-                      className="u-full-width" 
-                      type="number" 
-                      placeholder="Ejemplo: 3000" 
-                  />
-              </div>
-              <div>
-                  <label>Plazo para Pagar</label>
-                  <select 
-                      className="u-full-width"
-                  >
-                      <option value="">Seleccionar</option>
-                      <option value="3">3 meses</option>
-                      <option value="6">6 meses</option>
-                      <option value="12">12 meses</option>
-                      <option value="24">24 meses</option>
-                  </select>
-              </div>
-              <div>
-                  <input 
-                      type="submit" 
-                      value="Calcular" 
-                      className="button-primary u-full-width" 
-                  />
-              </div>
-          </div>
-  </form>
-     );
+const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
+
+   //estado local
+   const [error, guardarError] = useState(false);
+
+    //leer cantidad
+    const leerCantidad = (e)=>{
+        //console.log(typeof(e.target.value)) //es un dato string
+        //convertiremos a numero y le daremos eso a guardarCantidad:
+
+        guardarCantidad(parseInt(e.target.value));
+        console.log(guardarCantidad)
+    }
+
+    //calculo del precio
+    const calcularPrestamo = e =>{
+        e.preventDefault()
+        //console.log('calculando Precio')
+
+    //validar
+        if(cantidad ===0 || plazo===''){
+            console.log('Hay error');
+            guardarError(true);
+        }else{
+            //Realizar la cotizacion
+            guardarError(false); //elimina el error previo
+        }
+    
+    }
+
+    return (
+        <Fragment>
+        <form onSubmit={calcularPrestamo}>
+            <div className="row">
+                <div>
+                    <label>Cantidad Prestamo</label>
+                    <input
+                        className="u-full-width"
+                        type="number"
+                        placeholder="Ejemplo: 3000"
+                        //Agrego un evento de react
+                        onChange={leerCantidad}
+                    />
+                </div>
+                <div>
+                    <label>Plazo para Pagar</label>
+                    <select
+                        className="u-full-width"
+                        onChange={e =>{ guardarPlazo(parseInt(e.target.value)) }}
+                    >
+                        <option value="">Seleccionar</option>
+                        <option value="3">3 meses</option>
+                        <option value="6">6 meses</option>
+                        <option value="12">12 meses</option>
+                        <option value="24">24 meses</option>
+                    </select>
+                </div>
+                <div>
+                    <input
+                        type="submit"
+                        value="Calcular"
+                        className="button-primary u-full-width"
+                    />
+                </div>
+            </div>
+        </form>
+
+        {(error) ? <p className="error">Todos los campos son obligarios</p>: null}
+        
+        </Fragment>
+    );
 }
- 
+
 export default Formulario;
